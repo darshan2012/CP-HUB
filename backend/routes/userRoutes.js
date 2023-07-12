a061db5bc6ee14871653e4a42ae0cb0dca280353
Darshan <darshanparmar200d@gmail.com>
2023-07-13 00:55:13 +0530

    test

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,6 +14,6 @@ router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
-
+    
 
 export default router;
